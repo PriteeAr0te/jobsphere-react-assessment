660701371461ad79170bfacbf67ec50312fcfe88
PriteeAr0te <arotepritee111@gmail.com>
2025-07-28 22:49:34 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <h1 className='w-screen h-screen text-center mt-8 text-4xl font-bold text-white tracking-wide'>JobSphere</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
